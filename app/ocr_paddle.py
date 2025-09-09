8a448f3324c9862a7476356b64aac481cbc591e1
Marek Sabov <marek.sabov@gmail.com>
2025-09-09 22:53:32 +0200

    working sensor

--- app/ocr_paddle.py
+++ app/ocr_paddle.py
@@ -1,5 +1,10 @@
-import cv2, os
+# app/ocr_paddle.py
+import os, logging, cv2
 from paddleocr import PaddleOCR
+
+# stíšiť Paddle
+os.environ.setdefault("PPocr_DEBUG", "0")
+logging.getLogger("ppocr").setLevel(logging.WARNING)
 
 _reader = None
 
@@ -12,21 +17,17 @@ def get_reader():
 def prepare_roi(bgr, upscale=2):
     gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
     if upscale and upscale > 1:
-        gray = cv2.resize(gray, None, fx=upscale, fy=upscale,
-                          interpolation=cv2.INTER_CUBIC)
+        gray = cv2.resize(gray, None, fx=upscale, fy=upscale, interpolation=cv2.INTER_CUBIC)
     return gray
 
 def ocr_digits(bgr, upscale=2):
-    """Vráti (digits_str, conf) – digits_str je len [0-9] bez iného šumu."""
-    import re, numpy as np
+    import numpy as np
     roi = prepare_roi(bgr, upscale=upscale)
-    # Paddle má rád 3-kanál (BGR)
     roi3 = cv2.cvtColor(roi, cv2.COLOR_GRAY2BGR)
     res = get_reader().ocr(roi3, cls=False)
 
     best_text, best_conf = "", 0.0
     if res and res[0]:
-        # Poskladáme všetky detekované kusy do jedného
         joined = "".join([x[1][0] for x in res[0]])
         confs = [float(x[1][1]) for x in res[0]]
         best_text, best_conf = joined, float(np.mean(confs))
