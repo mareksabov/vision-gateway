4644d4b68bd1bd9813a25583810e66b559efa671
Marek Sabov <marek.sabov@gmail.com>
2025-09-17 16:13:16 +0200

    Added moquito script

--- mosquitto.sh
+++ mosquitto.sh
@@ -1,1 +1,7 @@
-mosquitto_sub -h 192.168.30.173 -p 1883 -u mqtt-user -P mqtt-pass -t "ha/electricity/#" -v
+#!/usr/bin/env bash
+set -euo pipefail
+
+ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$ROOT_DIR/secret/mqtt.env"
+
+mosquitto_sub -h "192.168.30.173" -p 1883 -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" -t 'ha/electricity/#' -v
